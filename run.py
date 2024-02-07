a816bc3b0bf4c6357bdf820aecb2c13351d17bef
gso5699 <gloria.jso5699@gmail.com>
2024-02-07 13:02:20 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+
+def run(
+# Include flags here 
+    # model:
+    train_model: bool = False,
+    test_model: bool = False,
+    output_path: Path = None,
+):
+
+    return
+    
